342c5569fddc834dd506b4f4e1eeae6e150fa5ac
Nayama Rajlich <design@nayamarajlich.com>
2024-01-21 20:55:11 -0800

    first commit NJS

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,44 +1,52 @@
+'use client'
 import { FiMenu } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 import Avatar from "./Avatar";
-import {useState} from 'react';
+import { useState } from 'react';
 const Navbar = () => {
-    const [nav, setNav] = useState=(false)
+    const [nav, setNav] = useState(true);
     const handleNav = () => {
-        setNav(!nav)
+        console.log('click')
+        setNav(!nav);
     }
-    return (
 
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-             
-            <ul className='font-poppins flex hidden '> 
-            <div className="p-4"><Avatar/></div>
-                <li className='p-4'>Home</li>
-                <li className='p-4'>My projects</li>
-                <li className='p-4'>About Me</li>
-                
-            </ul>
-            
-        <div/>
-      
-        <div>
-            <FiMenu className="text-white" size={25}/>
-            </div>
-        <div className=" font-poppins fixed left-0 top-4 w-[60%]  bg-[#1a1313]">
-        <div className="p-4"><Avatar/></div>
-            <ul className=" uppercase"> 
-         
+    let mobile = null;
+    if (!nav) {
+        mobile = <div className=" lg:hidden font-poppins fixed top-[100px] w-[60%]  bg-[#1a1313]">
+            <ul className=" uppercase">
+
                 <li className='p-4 border-b border-red-900'>Home</li>
                 <li className='p-4 border-b border-red-900'>My projects</li>
-                <li className='p-4 border-b border-red-900'>About Me</li>    
-        </ul>
-            
+                <li className='p-4 border-b border-red-900'>About Me</li>
+            </ul>
+
+ 
+        </div>
+    }
+
+    return (
+        <div className=' flex justify-between items-center h-24 pt-10 px-8 mx-auto text-white'>
+                            <div className="p-4"><Avatar /></div>
+
+            <ul className='cursor-pointer font-poppins hidden lg:flex'>
+                <li className='p-4 hover:underline '>Home</li>
+                <li className='p-4 hover:underline '>My projects</li>
+                <li className='p-4 hover:underline '>About Me</li>
+
+            </ul>
+
+            <div className="lg:hidden" onClick={handleNav}>
+                
+                {!nav ? <MdClose className="text-white" size={25} /> : <FiMenu size={25} />}
+            </div>
+
+            {mobile}
 
         </div>
-        </div>
 
-     
-            
-       
+
+
+
     )
 }
 export default Navbar;
