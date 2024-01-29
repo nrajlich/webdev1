78d7e945e5d89abfcb3f974c7bee6d4200abddf4
Nayama Rajlich <design@nayamarajlich.com>
2024-01-28 20:50:34 -0800

    pt sm adjustment

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Hero = () => (
-  <div className="justify-center md:pt-32 pt-24 lg:pt-32">
+  <div className="justify-center md:pt-32 pt-6 lg:pt-32">
     <div className="items-center text-black w-full mx-auto text-center text-l flex flex-col justify-center">
       <h1 className="lg:text-9xl md:text-8xl pr-1 pl-1 text-6xl tracking-tight font-bold font-inter-sans">
         I am a Designer.
@@ -9,7 +9,7 @@ const Hero = () => (
       <p className="pt-4 w-[320px] text-center font-poppins lg:hidden md:hidden">
         I transform underwhelming interfaces into modern and compelling designs. I create solutions that meet the unique needs of each project.
       </p>
-      <p className="pt-4 w-3/4 text-center font-poppins md:block hidden">
+      <p className="pt-4 w-3/4 text-center lg:w-[1000px] font-poppins md:block hidden">
         I transform underwhelming interfaces into modern and compelling designs. 
         I create solutions that meet the unique needs of each project. I enjoy being inspired, challenged, and pushed by exciting projects and people. Let's work together!
       </p>
