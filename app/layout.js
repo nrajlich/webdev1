300de89271cc27a3e90030df02c5d945a9ebd0aa
Nayama Rajlich <design@nayamarajlich.com>
2024-01-28 20:31:11 -0800

    grid

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.variable} ${roboto.variable} ${poppins.variable} ${GeistSans.variable}`}>
         <Navbar />
-        <Hero />
+        
         {children}
         
       </body>
