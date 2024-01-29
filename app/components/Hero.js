5bac5103ab3e5ec962f25361d9643d223cf09d9d
Nayama Rajlich <design@nayamarajlich.com>
2024-01-28 20:19:41 -0800

    pushing grid

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => (
   <div className="justify-center md:pt-32 pt-24 lg:pt-40">
     <div className="items-center text-black w-full mx-auto text-center text-l flex flex-col justify-center">
       <h1 className="lg:text-9xl md:text-8xl pr-1 pl-1 text-6xl tracking-tight font-bold font-inter-sans">
-        I am a Designer &amp;<br className="sm:hidden" /> <span className="inline-block">a Developer.</span>
+        I am a Designer &amp;<br className="sm:hidden" /> <span className="inline-block">Developer.</span>
       </h1>
       <p className="pt-4 w-[320px] text-center font-poppins lg:hidden md:hidden">
         I transform underwhelming interfaces into modern and compelling designs. I create solutions that meet the unique needs of each project.
